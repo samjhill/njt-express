2496945ff8a6b1d4f4cbb842eca4d3e2d2181850
Sam Hill <samuhill@gmail.com>
2020-02-15 15:53:50 -0500

    use older syntax for compatibility

--- index.mjs
+++ index.mjs
@@ -1,10 +1,9 @@
-import express from 'express';
-import * as NJTApi from 'njt-api';
-
+const express = require('express');
+const NJTApi = require('njt-api');
 const app = express();
-const port = 3000;
+const port = 80;
 
-const { Schedule, Stations } = NJTApi.default;
+const { Schedule, Stations } = NJTApi;
 
 const newarkStation = "Newark%20Broad%20Street";
 const pennStation = "New%20York%20Penn%20Station";
@@ -13,6 +12,10 @@ const getSchedule = async (fromStation, toStation) => {
   return Schedule.getScheduleForDay(fromStation, toStation, Date.now());
 };
 
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
 app.get('/schedule/from/:fromStation/to/:toStation', async (req, res) => {
   const { fromStation, toStation } = req.params;
