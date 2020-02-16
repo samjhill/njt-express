378303c8663e1caedf7284c05252472123c008ab
Sam Hill <samuhill@gmail.com>
2020-02-15 20:24:48 -0500

    styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 import BackgroundVideo from 'react-background-video-player';
 
 import { Box, Text } from "rebass";
+import { motion } from "framer-motion"
 
 const { NODE_ENV } = process.env;
 
@@ -18,10 +19,11 @@ const serverUrl = URLS[NODE_ENV] || URLS.default;
 const FloatingText = ({ children }) => (
   <Text 
     textAlign="left"
-    fontSize="6" 
+    fontSize="8" 
     color="white" 
+    fontFamily="'Roboto', sans-serif"
     sx={{
-      textShadow: "2px 2px #010101c7",
+      textShadow: "5px 5px #010101c7",
     }}
   >
     {children}
@@ -31,7 +33,9 @@ const FloatingText = ({ children }) => (
 function App() {
   const [schedule, setSchedule] = useState(null);
   const [videosList, setVideosList] = useState(null);
-  
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videoIsLoading, setVideoIsLoading] = useState(true);
+
   useEffect(() => {
     fetch(`${serverUrl}/schedule/from/Newark%20Broad%20Street/to/New%20York%20Penn%20Station`)
       .then(res => res.json())
@@ -49,6 +53,7 @@ function App() {
     .then(
       (result) => {
         setVideosList(result);
+        setSelectedVideo(result[Math.floor(Math.random() * result.length)])
       },
       (error) => {
         console.error(error);
@@ -68,33 +73,56 @@ function App() {
   const diff = departureTime.diff(moment());
   const diffDuration = moment.duration(diff);
 
-  const selectedVideo = videosList[Math.floor(Math.random() * videosList.length)];
-
   return (
     <div className="App">
-      <div style={{position: 'absolute', width: ' 100%', height: '100%'}}>
-        <BackgroundVideo 
-          playsInline={true}
-          containerWidth={100}
-          containerHeight={100}
-          src={`${serverUrl}/videos/${selectedVideo}`}
-          poster={''}
-          autoPlay={true}
-          volume={0}
-          style={{
-            zIndex: 0
-          }}
-        />
-      </div>
-      <Box 
-      m="4"
-      p="4"
-      style={{
-        zIndex: 1,
-        position: "absolute"
-      }}>
-        <FloatingText>Next Train</FloatingText>
-        <FloatingText>{diffDuration.minutes()} minutes - {departureTime.format('h:mma')}</FloatingText>
+        <Box>
+          <Box sx={{
+              backgroundColor: "#333", 
+              position: 'absolute', width: ' 100%', height: '100%'
+            }}>
+          <motion.div 
+            animate={
+              videoIsLoading
+                ? { opacity: 0 }
+                : { opacity: 1 }
+            }
+            transition={{ duration: 1 }}
+          >
+          <div style={{position: 'absolute', width: ' 100%', height: '100%'}}>
+            <BackgroundVideo 
+              playsInline={true}
+              containerWidth={100}
+              containerHeight={100}
+              src={`${serverUrl}/videos/${selectedVideo}`}
+              poster={''}
+              autoPlay={true}
+              volume={0}
+              style={{
+                zIndex: 0,
+              }}
+              loop={false}
+              onReady={() => {
+                setVideoIsLoading(false);
+              }}
+              onEnd={() => {
+                setVideoIsLoading(true);
+                setSelectedVideo(videosList[Math.floor(Math.random() * videosList.length)]);
+              }}
+            />
+          </div>
+          </motion.div>
+          </Box>
+        <Box 
+        m="4"
+        p="4"
+        style={{
+          zIndex: 1,
+          position: "absolute"
+        }}>
+          <FloatingText>Broad Street to Penn Station <strong>#{nextTrain.origin.trainNumber}</strong></FloatingText>
+
+          <FloatingText>leaves in <strong>{diffDuration.minutes()} minutes</strong>, at <strong>{departureTime.format('h:mma')}</strong>.</FloatingText>
+        </Box>
       </Box>
     </div>
   );
